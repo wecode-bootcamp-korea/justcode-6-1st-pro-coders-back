5ba162c7ef87a366a61c61b48f9e8855ab64ef9b
Youn-Sik Cho <108907032+younsik2@users.noreply.github.com>
2022-09-01 11:00:18 +0900

    1
    로그인,

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const userRouter = require('./user');
-const searchRouter = require('./search');
 // const otherRouter = require('./other');
 
 const router = express.Router();
 
-router.use('/users', userRouter);
-router.use('/search', searchRouter);
+router.use(userRouter);
+// router.use(otherRouter);
+const userRouter = require('./userRouter');
+const postRouter = require('./postRouter');
+
+router.use('/user', userRouter);
+router.use('/postings', postRouter);
 
 module.exports = router;
