92f1b9a3773d09083cb1516de4adf7ddf0e69e11
Youn-Sik Cho <108907032+younsik2@users.noreply.github.com>
2022-09-05 15:14:17 +0900

    re

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-require('dotenv').config();
-
-const http = require('http');
-const express = require('express');
-const cors = require('cors');
-
-const routes = require('./routes');
-<<<<<<< Updated upstream
-const { captureRejectionSymbol } = require('events');
-const fs = require('fs');
-=======
->>>>>>> Stashed changes
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-const server = http.createServer(app);
+require(‘dotenv’).config();
+const http = require(‘http’);
+const { createApp } = require(‘./app’);
+const server = http.createServer(createApp());
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
-  const dir = './uploads';
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
   console.log(`server start : http://localhost:${PORT}/`);
 });
-node
